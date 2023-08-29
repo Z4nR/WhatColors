9e6d15c1e67a97652ce12fe2dfa28e2651f8d881
Zulham Ari <ari.comunity1@gmail.com>
2023-08-29 22:11:12 +0700

    setup for PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,65 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/WhatColors/",
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        theme_color: "#ffffff",
+        background_color: "#008080",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        description: "WhatColors is Online Color Blind Test Website",
+        name: "WhatColors: ColorBlind Test",
+        short_name: "WhatColors",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        clientsClaim: true,
+        skipWaiting: true,
+        sourcemap: false,
+      },
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
     },
   },
-  base: "/",
 });
