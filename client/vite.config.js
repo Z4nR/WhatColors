d67058392926d49cb22c3600e8d6110f85ae5390
Zulham Ari <ari.comunity1@gmail.com>
2023-08-30 13:09:09 +0700

    update vite config icon manifest file

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,23 +20,23 @@ export default defineConfig({
         short_name: "WhatColors",
         icons: [
           {
-            src: "/pwa-64x64.png",
+            src: "/WhatColors/pwa-64x64.png",
             sizes: "64x64",
             type: "image/png",
           },
           {
-            src: "/pwa-192x192.png",
+            src: "/WhatColors/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/WhatColors/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "/maskable-icon-512x512.png",
+            src: "/WhatColors/maskable-icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
