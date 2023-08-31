32280bd06f12de9f3aa5009c91a9a45781d75deb
Z4nR <ari.comunity1@gmail.com>
2023-08-31 17:00:48 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/WhatColors/sw.js', { scope: '/WhatColors/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
