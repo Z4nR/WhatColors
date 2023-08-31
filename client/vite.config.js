b6589836c842bb8c7cc7d40d9f5e6bff4412dee5
Zulham Ari <ari.comunity1@gmail.com>
2023-09-01 00:13:15 +0700

    trying jsconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -56,6 +57,11 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
