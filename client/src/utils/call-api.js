7dd595ba89c741d113a74b8aa24396b99a1ae33d
Zulham Ari <ari.comunity1@gmail.com>
2023-09-08 12:22:38 +0700

    update env calling variable name

--- client/src/utils/call-api.js
+++ client/src/utils/call-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Server_URL = import.meta.env.VITE_BE_URL;
+const Server_URL = import.meta.env.VITE_BASE_URL;
 
 const newIndividual = async (userData) => {
   return axios
