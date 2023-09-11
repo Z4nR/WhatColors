89183fa0d63bc1b9fa7e3574631bd63651271feb
Zulham Ari <ari.comunity1@gmail.com>
2023-09-11 11:08:03 +0700

    update notfound page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Text, Button, useToast } from "@chakra-ui/react";
+import { Box, Heading, Text, Button, useToast, Flex } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 export default function NotFoundPage({ error }) {
   const toast = useToast();
@@ -14,31 +15,35 @@ export default function NotFoundPage({ error }) {
   });
 
   return (
-    <Box textAlign="center" py={10} px={6}>
-      <Heading
-        display="inline-block"
-        as="h2"
-        size="2xl"
-        bgGradient="linear(to-r, teal.400, teal.600)"
-        backgroundClip="text"
-      >
-        404
-      </Heading>
-      <Text fontSize="18px" mt={3} mb={2}>
-        Halaman Tidak Ditemukan
-      </Text>
-      <Text color={"gray.500"} mb={6}>
-        Halaman Yang Kamu Cari Tidak Dapat Ditemukan
-      </Text>
+    <Flex minHeight={"80vh"} justifyContent={"center"} alignItems={"center"}>
+      <Box textAlign="center" py={10} px={6}>
+        <Heading
+          display="inline-block"
+          as="h2"
+          size="2xl"
+          bgGradient="linear(to-r, teal.400, teal.600)"
+          backgroundClip="text"
+        >
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Halaman Tidak Ditemukan
+        </Text>
+        <Text color={"gray.500"} mb={6}>
+          Halaman Yang Kamu Cari Tidak Dapat Ditemukan
+        </Text>
 
-      <Button
-        colorScheme="teal"
-        bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
-        color="white"
-        variant="solid"
-      >
-        Kembali Ke Beranda
-      </Button>
-    </Box>
+        <Button
+          as={ReactRouterLink}
+          to={"/"}
+          colorScheme="teal"
+          bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+          color="white"
+          variant="solid"
+        >
+          Kembali Ke Beranda
+        </Button>
+      </Box>
+    </Flex>
   );
 }
