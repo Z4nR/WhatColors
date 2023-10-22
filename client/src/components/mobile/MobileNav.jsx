bcb186aa19b0b8c5bf39c2b8018ca92572c7c746
Zulham Ari <ari.comunity1@gmail.com>
2023-10-22 11:53:09 +0700

    updt mobile botnav

--- client/src/components/mobile/MobileNav.jsx
+++ client/src/components/mobile/MobileNav.jsx
@@ -1,7 +1,7 @@
-import { InfoOutlineIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
-import { Box, HStack, Button, Icon } from "@chakra-ui/react";
-import { FiFileText, FiHome } from "react-icons/fi";
-import { useLocation, Link as ReactRouterLink } from "react-router-dom";
+import { InfoOutlineIcon, QuestionOutlineIcon } from '@chakra-ui/icons';
+import { Box, HStack, Button, Icon, VStack, Text } from '@chakra-ui/react';
+import { FiFileText, FiHome } from 'react-icons/fi';
+import { useLocation, Link as ReactRouterLink } from 'react-router-dom';
 
 export default function MobileNav() {
   const urlLocation = useLocation();
@@ -9,84 +9,84 @@ export default function MobileNav() {
 
   return (
     <Box
-      bg={"white"}
+      bg={'white'}
       bottom={0}
       left={0}
       right={0}
       as="footer"
-      position={"sticky"}
-      boxShadow={"0px 0px 2px 0px rgba(0, 0, 0, 0.2)"}
+      position={'sticky'}
+      boxShadow={'0px 0px 2px 0px rgba(0, 0, 0, 0.2)'}
       zIndex={11}
-      width={"full"}
+      width={'full'}
     >
-      <HStack p={4} gap={"5"} justifyContent={"center"}>
+      <HStack px={4} py={2} gap={'5'} justifyContent={'center'}>
         <Button
-          size={"xs"}
-          fontSize="sm"
           as={ReactRouterLink}
           to="/"
-          variant={"ghost"}
+          variant={'ghost'}
           aria-label="Home button"
-          aria-current={urlPath === "/" ? "page" : undefined}
+          aria-current={urlPath === '/' ? 'page' : undefined}
           sx={{
-            "&[aria-current=page]": {
-              color: "teal",
+            '&[aria-current=page]': {
+              color: 'teal',
             },
           }}
-          leftIcon={<Icon as={FiHome} />}
         >
-          Beranda
+          <VStack gap={1}>
+            <Icon fontSize={'lg'} as={FiHome} />
+            <Text fontSize={'xs'}>Beranda</Text>
+          </VStack>
         </Button>
         <Button
-          size={"xs"}
-          fontSize="sm"
           as={ReactRouterLink}
           to="/about"
-          variant={"ghost"}
+          variant={'ghost'}
           aria-label="About button"
-          aria-current={urlPath === "/about" ? "page" : undefined}
+          aria-current={urlPath === '/about' ? 'page' : undefined}
           sx={{
-            "&[aria-current=page]": {
-              color: "teal",
+            '&[aria-current=page]': {
+              color: 'teal',
             },
           }}
-          leftIcon={<Icon as={InfoOutlineIcon} />}
         >
-          Tentang
+          <VStack gap={1}>
+            <Icon fontSize={'lg'} as={InfoOutlineIcon} />
+            <Text fontSize={'xs'}>Tentang</Text>
+          </VStack>
         </Button>
         <Button
-          size={"xs"}
-          fontSize="sm"
           as={ReactRouterLink}
           to="/article"
-          variant={"ghost"}
+          variant={'ghost'}
           aria-label="Article button"
-          aria-current={urlPath === "/article" ? "page" : undefined}
+          aria-current={urlPath === '/article' ? 'page' : undefined}
           sx={{
-            "&[aria-current=page]": {
-              color: "teal",
+            '&[aria-current=page]': {
+              color: 'teal',
             },
           }}
-          leftIcon={<Icon as={FiFileText} />}
         >
-          Artikel
+          <VStack gap={1}>
+            <Icon fontSize={'lg'} as={FiFileText} />
+            <Text fontSize={'xs'}>Artikel</Text>
+          </VStack>
         </Button>
         <Button
-          size={"xs"}
-          fontSize="sm"
           as={ReactRouterLink}
           to="/faq"
-          variant={"ghost"}
+          variant={'ghost'}
           aria-label="FAQ button"
-          aria-current={urlPath === "/faq" ? "page" : undefined}
+          aria-current={urlPath === '/faq' ? 'page' : undefined}
           sx={{
-            "&[aria-current=page]": {
-              color: "teal",
+            '&[aria-current=page]': {
+              color: 'teal',
             },
           }}
-          leftIcon={<Icon as={QuestionOutlineIcon} />}
         >
-          FAQ
+          <VStack gap={1}>
+            <Icon fontSize={'lg'} as={QuestionOutlineIcon} />
+            <Text fontSize={'xs'}>FAQ</Text>
+          </VStack>
         </Button>
       </HStack>
     </Box>
