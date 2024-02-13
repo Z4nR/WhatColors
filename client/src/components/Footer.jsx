f01b4751538d64159f60adfe7861c41152948b6c
Z4NR <ari.comunity1@gmail.com>
2024-02-13 07:55:59 +0700

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,27 +1,16 @@
-import { Box, Flex, HStack, Link, Text } from "@chakra-ui/react";
-import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
+import { Box, Text } from '@chakra-ui/react';
 
 export default function Footer() {
+  const copyrightYear = new Date().getFullYear();
+
   return (
-    <Box as="footer" display={"block"} width={"full"}>
-      <Box width={"full"} bg={"teal"} color={"white"}>
-        <Flex direction={"column"} gap={6} pt={6} pb={8} alignItems={"center"}>
-          <Text>Created by Zulham 👋</Text>
-          <HStack spacing={8} justifyContent={"center"}>
-            <Link href="mailto: ari.comunity1@gmail.com" isExternal>
-              <FiMail />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/zulham-ari-nur-ridhwan"
-              isExternal
-            >
-              <FiLinkedin />
-            </Link>
-            <Link href="https://github.com/Z4nR" isExternal>
-              <FiGithub />
-            </Link>
-          </HStack>
-        </Flex>
+    <Box as="footer" display={'block'} width={'full'}>
+      <Box width={'full'} bg={'teal'} color={'white'} pt={6} pb={8}>
+        <Text align={'center'} fontSize={'smaller'}>
+          &copy; {copyrightYear}
+          <br />
+          Created by Ijan
+        </Text>
       </Box>
     </Box>
   );
