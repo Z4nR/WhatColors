bf36e3ef3c354fbb66b25cbdf7b33601f66f6e1d
Z4NR <ari.comunity1@gmail.com>
2024-03-03 03:28:29 +0700

    update not found error message page

--- client/src/components/utils/NotFound.jsx
+++ client/src/components/utils/NotFound.jsx
@@ -1,14 +1,9 @@
-import { useToastMsg } from "@/utils/customHooks";
-import { Box, Heading, Text, Button, Flex } from "@chakra-ui/react";
-import { Link as ReactRouterLink } from "react-router-dom";
+import { Box, Heading, Text, Button, Flex } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 export default function NotFound({ error }) {
-  const toast = useToastMsg();
-
-  toast("Terjadi Kesalahan", `${error.response.data.message}`, "error");
-
   return (
-    <Flex minHeight={"80vh"} justifyContent={"center"} alignItems={"center"}>
+    <Flex minHeight={'80vh'} justifyContent={'center'} alignItems={'center'}>
       <Box textAlign="center" py={10} px={6}>
         <Heading
           display="inline-block"
@@ -17,23 +12,23 @@ export default function NotFound({ error }) {
           bgGradient="linear(to-r, teal.400, teal.600)"
           backgroundClip="text"
         >
-          404
+          {error.response.data.status}
         </Heading>
         <Text fontSize="18px" mt={3} mb={2}>
-          Halaman Tidak Ditemukan
+          Sepertinya ada yang tidak beres
         </Text>
-        <Text color={"gray.500"} mb={6}>
-          Halaman Yang Kamu Cari Tidak Dapat Ditemukan
+        <Text color={'gray.500'} mb={6}>
+          {error.response.data.message}
         </Text>
 
         <Button
           as={ReactRouterLink}
-          to={"/"}
+          to={'/'}
           colorScheme="teal"
           bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
           color="white"
           variant="solid"
-          size={{ base: "xs", xs: "sm", md: "md" }}
+          size={{ base: 'xs', xs: 'sm', md: 'md' }}
         >
           Kembali Ke Beranda
         </Button>
